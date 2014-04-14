2eda2d1572a42e94877d9b2fe000ce496f8b70f7
Georgi Mateev <gmmateev92@abv.bg>
2014-04-14 07:58:55 +0300

    Fixed the query of donation items

--- wp-content/themes/responsive-child/content-campaign.php
+++ wp-content/themes/responsive-child/content-campaign.php
@@ -11,15 +11,14 @@
 	</div><!-- .entry-summary -->
 	<?php else : ?>
 	<div class="entry-content">
-		<?php
-			the_content( __(  'Read more &#8250;', 'responsive' ) );
-			?>
-			<h2>Custom Fields</h2>
-                        <img src="<?php print_custom_field('avatar:to_image_src'); ?>" /><br />
-                        
-                        <?php get_template_part('donation-items-list' ); ?>
-                        
-                        <a href='<?php echo site_url() . "/new-donation?campaign=" . get_the_ID(); ?>' >Дари!</a>
+            <img src="<?php print_custom_field('avatar:to_image_src'); ?>" /><br />
+            
+            <?php the_content( __(  'Read more &#8250;', 'responsive' ) );	?>
+            
+            <h4>We need:</h4>
+            <?php get_template_part('donation-items-list' ); ?>
+
+            <a href='<?php echo site_url() . "/new-donation?campaign=" . get_the_ID(); ?>' >Дари!</a>
 	</div><!-- .entry-content -->
 	<?php endif; ?>
 </article><!-- #post-## -->
--- wp-content/themes/responsive-child/donation-items-list.php
+++ wp-content/themes/responsive-child/donation-items-list.php
@@ -5,45 +5,44 @@
  */
 ?>
 
-<?php $donation_items_loop = new WP_Query( array( 'post_type' => 'donation-item', 'posts_per_page' => 100, 'parent' => get_the_ID() ) );?>
+<?php $donation_items_loop = new WP_Query( array( 'post_type' => 'donation-item',
+                                                  'posts_per_page' => 100,
+                                                  'meta_key' => 'parent',
+                                                  'meta_value' => get_the_ID() ) );?>
 
 <?php if( $donation_items_loop->have_posts() ) : ?>
 
-		<?php get_template_part( 'loop-header' ); ?>
+        <?php while( $donation_items_loop->have_posts() ) : $donation_items_loop->the_post(); ?>
 
-		<?php while( $donation_items_loop->have_posts() ) : $donation_items_loop->the_post(); ?>
+                <?php responsive_entry_before(); ?>
+                <div id="item-<?php the_ID(); ?>" <?php post_class(); ?>>
+                        <?php responsive_entry_top(); ?>
 
-			<?php responsive_entry_before(); ?>
-			<div id="item-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<?php responsive_entry_top(); ?>
+                        <div class="post-entry">
+                                <?php if( has_post_thumbnail() ) : ?>
+                                        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                                                <?php the_post_thumbnail( 'thumbnail', array( 'class' => 'alignleft' ) ); ?>
+                                        </a>
+                                <?php endif; ?>
 
-				<div class="post-entry">
-					<?php if( has_post_thumbnail() ) : ?>
-						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-							<?php the_post_thumbnail( 'thumbnail', array( 'class' => 'alignleft' ) ); ?>
-						</a>
-					<?php endif; ?>
-					
-                                        <?php get_template_part('content', 'donation-item' ); ?>
-                                    
-					<?php wp_link_pages( array( 'before' => '<div class="pagination">' . __( 'Pages:', 'responsive' ), 'after' => '</div>' ) ); ?>
-				</div>
-				<!-- end of .post-entry -->
+                                <?php get_template_part('content', 'donation-item' ); ?>
 
-				<?php //get_template_part('post-data' ); ?>				
+                                <?php wp_link_pages( array( 'before' => '<div class="pagination">' . __( 'Pages:', 'responsive' ), 'after' => '</div>' ) ); ?>
+                        </div>
+                        <!-- end of .post-entry -->
 
-				<?php responsive_entry_bottom(); ?>
-			</div><!-- end of #item-<?php the_ID(); ?> -->
-			<?php responsive_entry_after(); ?>
+                        <?php //get_template_part('post-data' ); ?>				
 
-		<?php
-		endwhile;
+                        <?php responsive_entry_bottom(); ?>
+                </div><!-- end of #item-<?php the_ID(); ?> -->
+                <?php responsive_entry_after(); ?>
 
-		get_template_part( 'loop-nav' );
+        <?php
+        endwhile;
 
-	else :
+else :
 
-		get_template_part( 'loop-no-posts' );
+        get_template_part( 'loop-no-posts' );
 
-	endif;
-	?>
+endif;
+?>
