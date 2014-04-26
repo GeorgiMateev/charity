dd3c1b6b45e0940860a9fb1a6af58a53d8ae5e84
Georgi Mateev <gmmateev92@abv.bg>
2014-04-26 23:55:26 +0300

    Extracted campaigns list logic in a new file

--- wp-content/themes/responsive-child/archive-campaign.php
+++ wp-content/themes/responsive-child/archive-campaign.php
@@ -22,52 +22,14 @@ get_header(); ?>
     
 		<?php while( $loop->have_posts() ) : $loop->the_post(); ?>
 
-			<?php responsive_entry_before(); ?>
-			<div class="content-wrapper">
-
-			<div class='progress-wrapper'>
-	  			<div class="progress-slider"></div>
-	 		<div class="progress-inner">
-	 			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
-					<?php responsive_entry_top(); ?>
-
-					<?php get_template_part( 'post-meta' ); ?>
-				
-					<div class="post-entry">
-						<?php if( has_post_thumbnail() ) : ?>
-							<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-								<?php the_post_thumbnail( 'thumbnail', array( 'class' => 'alignleft' ) ); ?>
-							</a>
-						<?php endif; ?>
-					
-                                        <?php get_template_part('campaign-preview' ); ?>
-                                    
-						<?php wp_link_pages( array( 'before' => '<div class="pagination">' . __( 'Pages:', 'responsive' ), 'after' => '</div>' ) ); ?>
-					</div>
-
-				</div>
-
-	 			</div>
-				<div class='progress-circle-outer'></div> 
-	 			<div class="progress-circle-inner"></div>
-	 			<img src='http://3cky.com/slider-bg.png' alt='slider background'/ class='slider-bg' />
-	 			<img src='http://3cky.com/slider-bg-up.png' alt='slider background up'/ class='slider-bg-up'>
-			</div>  
-
-				<!-- end of .post-entry -->
-
-				<?php get_template_part('post-data' ); ?>				
-
-				<?php responsive_entry_bottom(); ?>
-			</div><!-- end of #post-<?php the_ID(); ?> -->
-			<?php responsive_entry_after(); ?>
-
-		<?php
+		<?php get_template_part( 'campaigns-loop-content' ); ?>
+    
+        <?php
 		endwhile;
 
 		get_template_part( 'loop-nav' );
 
-	else :
+                else :
 
 		get_template_part( 'loop-no-posts' );
 
---
+++ wp-content/themes/responsive-child/archive.php
@@ -0,0 +1,48 @@
+<?php
+
+// Exit if accessed directly
+if( !defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Archive Template
+ *
+ *
+ * @file           archive.php
+ * @package        Responsive
+ * @author         Emil Uzelac
+ * @copyright      2003 - 2014 CyberChimps
+ * @license        license.txt
+ * @version        Release: 1.1
+ * @filesource     wp-content/themes/responsive/archive.php
+ * @link           http://codex.wordpress.org/Theme_Development#Archive_.28archive.php.29
+ * @since          available since Release 1.0
+ */
+
+get_header(); ?>
+
+<div id="content-archive" class="<?php echo implode( ' ', responsive_get_content_classes() ); ?>">
+
+	<?php if( have_posts() ) : ?>
+
+		<?php get_template_part( 'loop-header' ); ?>
+
+		<?php while( have_posts() ) : the_post(); ?>
+
+		<?php get_template_part("campaigns-loop-content")?>	
+
+		<?php
+		endwhile;
+
+		get_template_part( 'loop-nav' );
+
+	else :
+
+		get_template_part( 'loop-no-posts' );
+
+	endif;
+	?>
+
+</div><!-- end of #content-archive -->
+<?php get_footer(); ?>
