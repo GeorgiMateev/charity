6b552c3ef6371439d6e858d251d6dc4bbd45b359
Georgi Mateev <gmmateev92@abv.bg>
2014-04-26 15:02:21 +0300

    Donation success screen

--- wp-content/themes/responsive-child/donation.php
+++ wp-content/themes/responsive-child/donation.php
@@ -9,11 +9,74 @@
 
 <?php get_header(); ?>
 
-<h1>Браво!</h1>
+<?php
+    $campaign_id = $_POST["campaign_id"];
+    
+    global $post;
+    $post = get_post($campaign_id);
+?>
+
+<h1>Благодарим ви, че помагате!</h1>
+
+<?php
+    function generatePassword($length = 8) {
+        $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        $count = mb_strlen($chars);
+
+        for ($i = 0, $result = ''; $i < $length; $i++) {
+            $index = rand(0, $count - 1);
+            $result .= mb_substr($chars, $index, 1);
+        }
+
+        return $result;
+    }
+?>
+
+<h2><?php echo generatePassword(); ?></h2>
+<p>Изпратете този код заедно с доставката, за да сме сигурни, че даренето е получено.</p>
+
+<p>Може да изпратите вашето дарение на някой от посочените адреси:</p>
+
+<ul>
+    <?php foreach(get_custom_field("addresses") as $value){ 
+        echo "<li>";        
+            echo "<p>$value</p>";
+        echo "</li>";
+    } ?>
+</ul>
 
 <?php echo $_POST["donation_item"]; ?>
 <?php echo $_POST["donation_items_count"]; ?>
 
+<?php if(get_custom_field("home_visit")): ?>
+    <div>
+        <p>Желаете ли да дойдем и да вземем дарението от Вашия дом?</p>
+        <span>Да!</span><input id="home-visit" type="checkbox"/>
+        <div id="donator-contacts" style="display: none">
+            <p>Телефон:</p>
+            <input type="text" />
+            
+            <p>Три имена</p>
+            <input type="text" />
+            
+            <p>Вашият адрес</p>
+            <textarea></textarea><br />
+            
+            <input type="submit" />
+        </div>
+    </div>
+<?php endif; ?>
+
+<script type="text/javascript">
+    (function($){
+        $(function(){
+            $("#home-visit").change(function(){
+               $("#donator-contacts").toggle();
+            });
+    });
+    }(jQuery));   
+    
+</script>
 
 <?php get_footer(); ?>
 
