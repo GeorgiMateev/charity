d6c869234e83279673deada86c7b3230db6e2c75
Daniel Grigorov <too3cky@yahoo.com>
2014-04-27 11:37:08 +0300

    sidebar changes

--- wp-content/themes/responsive-child/archive-campaign.php
+++ wp-content/themes/responsive-child/archive-campaign.php
@@ -19,18 +19,25 @@ get_header(); ?>
 		<?php get_template_part( 'loop-header' ); ?>
                 
         <div class="sorting-wrapper">
-            <span>Филтрирай по</span>
-            <ul>
-                <li><a href="#">място</a></li>
-                <li><a href="#">срок</a></li>
+            
+            <span class='filter-title'>Филтрирай по:</span>
+
+            <ul><div class="tags-wrapper aside-wrapper">      
+                <li><a href="#">
+
+                 <div class="addresses-wrapper"><span class="addresses-icon"></span>място</div></a></li>
+                
+                <li><a href="#">
+                    <div class="end-date-wrapper"> <span class="end-date-icon"></span>
+                  срок</div></a></li>
                 <li>
-                    <a href="#">категория</a>
+                    <a href="#">   <div class='cat-wrapper'><span class="cat-icon"></span> категория</div></a>
                     <?php the_widget( 'WP_Widget_Categories', 'title= ' ); ?> 
                 </li>
             </ul>
         </div>
                 
-    
+ 
 		<?php while( $loop->have_posts() ) : $loop->the_post(); ?>
 
 		<?php get_template_part( 'campaigns-loop-content' ); ?>
